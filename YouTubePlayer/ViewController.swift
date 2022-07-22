6d210aaa5f85c4101fbde6a29cbc55e6e7965cef
Rashid Gaitov <rashidgaitov@gmail.com>
2022-07-22 14:38:05 +0300

    Added 2 video players

--- YouTubePlayer/ViewController.swift
+++ YouTubePlayer/ViewController.swift
@@ -5,13 +5,21 @@
 //  Created by Rashid Gaitov on 22.07.2022.
 //
 
+import youtube_ios_player_helper
 import UIKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var videoPlayerView: YTPlayerView!
+    
+    @IBOutlet var secondVideoPlayerView: YTPlayerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        videoPlayerView.load(withVideoId: "ARCxyt9GS5o")
+        secondVideoPlayerView.load(withPlaylistId: "PLsUMoyJKBqcn7dk3jC3i1023Ie-BntpgF")
     }
 
 
